c153b0060dd51d539ee3a728b608dc6a06471eab
Niobos <niobos@dest-unreach.be>
2011-08-21 12:55:36 +0200

    Documented test during runtime

--- test/slow-client.sh
+++ test/slow-client.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 PORT=61234
+TIME=15
+BANDWIDTH=100000
 
+echo "Testing behaviour when a slow client is connected for $TIME seconds..."
 mkfifo pipe.$$
 
 ../src/velbusd -s pipe.$$ -b [::1]:[${PORT}] >/dev/null 2>&1 &
@@ -13,7 +16,7 @@ socat TCP6:localhost:$PORT,rcvbuf=1024 EXEC:"sleep 15" &
 PID_SLOW=$!
 
 # Spit 1.4MB/second
-( while sleep 1; do i=0; while [ $i -lt 100000 ]; do echo -ne "\x0f\xfb\x02\x08\xfb\x01\x00\x00\x00\x00\x00\x00\xf0\x04"; i=$(($i+1)); done; done ) |
+( while sleep 1; do i=0; while [ $i -lt $BANDWIDTH ]; do echo -ne "\x0f\xfb\x02\x08\xfb\x01\x00\x00\x00\x00\x00\x00\xf0\x04"; i=$(($i+1)); done; done ) |
 	./NBcat | socat - TCP6:localhost:$PORT > /dev/null
 RV=$?
 
