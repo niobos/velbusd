2904694ca9b7863ad0fe2fdc4108c45dab470d6b
Niobos <niobos@dest-unreach.be>
2011-08-23 21:52:22 +0200

    Added composing check to parsers

--- test/Parser.cpp
+++ test/Parser.cpp
@@ -8,10 +8,17 @@ int main(int argc, char* argv[]) {
 	std::cin.read(buffer,sizeof(buffer));
 
 	std::string data(buffer, std::cin.gcount() );
+	std::string data_copy(data);
 
 	try {
 		std::auto_ptr<VelbusMessage::VelbusMessage> m( VelbusMessage::parse_and_consume( data ) );
 		std::cout << m->string() << "\n";
+
+		std::string b = m->message();
+		if( b.compare(data_copy) != 0 ) {
+			std::cout << "message() does not return same data as passed in\n";
+			return 1;
+		}
 
 	} catch( VelbusMessage::InsufficientData &e ) {
 		std::cout << "InsufficientData\n";
