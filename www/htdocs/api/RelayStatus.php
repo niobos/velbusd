8bf5a5fadd44193a2b49d592237ea77ad27777fb
Niobos <niobos@dest-unreach.be>
2011-09-22 22:03:41 +0200

    Added RelayStatus Get request

--- www/htdocs/api/RelayStatus.php
+++ www/htdocs/api/RelayStatus.php
@@ -9,13 +9,14 @@ if( preg_match("%^/([0-9a-fA-F]{2})\.(\d)$%", $_SERVER["PATH_INFO"], $matches) =
 $addr = hexdec($matches[1]);
 $relay = (int)($matches[2]);
 
+if( $relay > 4 ) fail('Invalid relay number');
 
 $sock = socket_create(AF_INET6, SOCK_STREAM, 0);
 if( $sock === FALSE ) trigger_error("Could not create backend socket", E_ERROR_USER);
 
-//if( socket_connect($sock, $config["host"], $config["port"]) === FALSE ) {
-//	trigger_error("Could not connect", E_USER_ERROR);
-//}
+if( socket_connect($sock, $config["host"], $config["port"]) === FALSE ) {
+	trigger_error("Could not connect", E_USER_ERROR);
+}
 
 switch( $_SERVER["REQUEST_METHOD"] ) {
 case "POST":
@@ -37,8 +38,20 @@ case "GET":
 		"id" => "$addr.$relay",
 		"addr" => $addr,
 		"relay" => $relay,
-		"state" => "on",
 		);
+	$msg = message($addr, 3, 0, chr(0xfa) . chr(1 << $relay) );
+	if( socket_send($sock, $msg, strlen($msg), 0) != strlen($msg) )
+		trigger_error("Send failed", E_ERROR_USER);
+	$msg = expect_answer($sock, $addr, "\xfb".chr(1 << $relay), 1);
+
+	if( $msg !== NULL ) {
+		switch( ord($msg[7]) ) {
+		case 0x00: $ret["state"] = "off"; break;
+		case 0x01: $ret["state"] = "on"; break;
+		case 0x03: $ret["state"] = "interval"; break;
+		}
+	}
+
 	print json_encode($ret);
 	break;
 
--- www/htdocs/api/velbus.php
+++ www/htdocs/api/velbus.php
@@ -26,57 +26,60 @@ function message($addr, $prio, $rtr, $data) {
 	return $msg;
 }
 
-function expect_answer($sock, $addr, $cmd_filter=NULL) {
+function read_message($sock, $timeout=1) {
+	$read = array($sock);
+	$write = NULL; $except = NULL;
 
-}
-
-function send_command($addr, $prio, $rtr, $data, $answer_cmd=NULL) {
-	$msg = message($addr, $prio, $rtr, $data);
-
-	$sock = socket_create(AF_INET6, SOCK_STREAM, 0);
-	if( $sock === FALSE ) trigger_error("Could not create backend socket", E_USER_ERROR);
-
-	if( socket_set_option($sock, SOL_SOCKET, SO_RCVTIMEO, array("sec"=>1,"usec"=>0) ) === FALSE )
-		trigger_error("Could not set_socket_opt()", E_USER_ERROR);
-
-	if( socket_connect($sock, "::1", 8445) === FALSE ) trigger_error("Could not connect", E_USER_ERROR);
-
-	if( socket_send($sock, $msg, strlen($msg), 0) != strlen($msg) ) trigger_error("Send failed", E_USER_ERROR);
-
-	$answer = NULL;
-	$timeout = microtime(true)+1;
 	$msg = "";
-	while( $answer_cmd != NULL && microtime(true) < $timeout ) { 
+	$start = microtime(true);
+
+	$bytes_needed = 6;
+	while( socket_select($read, $write, $except, floor($timeout), floor($timeout*1000000 % 1000000) ) ) {
+		$timeout -= microtime(true) - $start;
+
 		$buf = "";
-		if( socket_recv($sock, $buf, 128, 0) === FALSE ) trigger_error("Recv failed", E_USER_ERROR);
+		if( socket_recv($sock, $buf, $bytes_needed, 0) === FALSE ) trigger_error("Recv failed", E_USER_ERROR);
 
 		$msg .= $buf;
-
-		while(1) {
-			if( strlen($msg) < 6 ) continue 2; # gather more data
-
-			$a = str_split($msg);
-			if( $a[0] != "\x0f" ) trigger_error("Unsynchronized read, should not happen", E_USER_ERROR);
-
-			$len = ord($a[3]) & 0x0f;
-			if( strlen($msg) < 6+$len ) continue 2; # gather more data
-
-			# consume message; copy is in $a anyway
-			$thismsg = substr($msg, 0, 6+$len);
-			$msg = substr($msg, 6+$len);
-			
-			if( ord($a[2]) != $addr ) continue 1; # read next message
-			if( ord($a[4]) != $answer_cmd ) continue 1;
-
-			$answer = $thismsg;
-			break 2;
+		if( strlen($msg) < 6 ) {
+			$bytes_needed -= strlen($buf);
+			continue; # gather more data
 		}
+
+		$a = str_split($msg);
+		if( $a[0] != "\x0f" ) trigger_error("Unsynchronized read, should not happen", E_USER_ERROR);
+
+		$len = ord($a[3]) & 0x0f;
+		if( strlen($msg) < 6+$len ) {
+			$bytes_needed = $len;
+			continue; # gather more data
+		}
+
+		return $msg;
 	}
-
-	socket_close($sock);
-
-	return $answer;
+	return NULL;
 }
 
+function expect_answer($sock, $addr, $msg_filter=NULL, $timeout=1) {
+	$start = microtime(true);
+
+	while( $timeout > 0 ) {
+		$msg = read_message($sock, $timeout);
+
+		if( $msg == NULL ) return NULL; // Timeout
+
+		$timeout -= microtime(true) - $start; // Correct timeout
+
+		$a = str_split($msg);
+		if( ord($a[2]) != $addr ) continue; # read next message
+		if( $msg_filter != NULL ) {
+			for( $i = 0; $i < strlen($msg); $i++ ) {
+				if( $a[4+$i] != $msg_filter[$i] ) continue 2;
+			}
+		}
+
+		return $msg;
+	}
+}
 
 ?>
