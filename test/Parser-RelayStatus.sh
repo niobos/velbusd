c8083975383e4df788120a86ba284938eca94be2
Niobos <niobos@dest-unreach.be>
2011-10-23 21:17:10 +0200

    Leftover change from efbb892199fe6334dbbadaa7d6bd1b08bfe9fe16

--- test/Parser-RelayStatus.sh
+++ test/Parser-RelayStatus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 OUTPUT="$( echo -ne "\x0f\xfb\x02\x08\xfb\x01\x00\x00\x80\x00\x00\x00\x70\x04" | ./Parser )"
-test "$OUTPUT" == "RelayStatus of 0x02 / 0 : relay=off status=normal LED=on timer=0"
+test "$OUTPUT" == "RelayStatus of 0x02 / 1 : relay=off status=normal LED=on timer=0"
 if [ $? -ne 0 ]; then
 	echo "Wrong output: $OUTPUT"
 	exit 1
