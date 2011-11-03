bd4a8bb2b2a52ffc75d523bd2998f059ce4cffd2
Niobos <niobos@dest-unreach.be>
2011-11-03 20:09:46 +0100

    Added RTC generator to sync Velbus devices to PC

---
+++ bin/gen-RTCstatus.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+sub checksum ($) {
+	my $sum = unpack("%8C*", $_[0]);
+	return chr( 256-$sum );
+}
+
+my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
+
+$wday--; $wday = 6 if $wday == -1; # convert 0=Sunday to 0=Monday
+
+my $rtc_message = "\x0f\xfb\x00\x04\xd8" . chr($wday) . chr($hour) . chr($min);
+$rtc_message .= checksum($rtc_message);
+$rtc_message .= "\x04";
+
+print $rtc_message;
