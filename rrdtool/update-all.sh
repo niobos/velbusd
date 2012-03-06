bd142272a23a691cb33be49f3b5e3108b642d4bc
Niobos <niobos@dest-unreach.be>
2012-03-06 19:57:34 +0100

    Made update-all script find it's companions more flexible

--- rrdtool/update-all.sh
+++ rrdtool/update-all.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+: ${TOOLPATH:=$(cd $(dirname "$0"); echo $PWD)}
+
 cd ${RRDDIR:-/var/lib/rrd/} || exit 1
 
 for f in *-TS.rrd; do
 	ADDR="${f%%-TS.rrd}"
 	echo "Updating TS $ADDR"
-	./update-TS.sh "$ADDR"
+	"$TOOLPATH/update-TS.sh" "$ADDR"
 done
 
 for f in *-RELAY.rrd; do
 	ADDR="${f%%-RELAY.rrd}"
 	echo "Updating RELAY $ADDR"
-	./update-RELAY.sh "$ADDR"
+	"$TOOLPATH/update-RELAY.sh" "$ADDR"
 done
