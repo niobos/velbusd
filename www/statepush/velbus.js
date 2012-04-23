d2282061a9218c142ec5d0a302d5540026186751
Niobos <niobos@dest-unreach.be>
2012-04-23 21:00:20 +0200

    Split off velbus into separate module

---
+++ www/statepush/velbus.js
@@ -0,0 +1,94 @@
+var events = require('events'),
+    util = require('util'),
+    net = require('net');
+
+var velbus = function(host, port) {
+	var that = this;
+	events.EventEmitter.call(this); // Inherit EventEmitter
+	that.host = host;
+	that.port = port;
+	that.retry_after_ms = 1000;
+
+	that.socket = net.connect(that.port, that.host, function() {
+		that.emit('connect');
+	});
+
+	that.socket.on('error', function (e) {
+		that.emit('error', e);
+	});
+
+	that.socket.on('close', function() {
+		that.emit('close');
+		setTimeout(function(){
+				that.socket.connect(that.host, that.port);
+			}, that.retry_after_ms);
+	});
+
+	that.unprocessed_data = new Buffer(4096);
+	that.n_unprocessed_data = 0;
+
+	that.socket.on('data', function(data) {
+		data.copy(that.unprocessed_data, that.n_unprocessed_data);
+		that.n_unprocessed_data += data.length;
+		that.process_data();
+	});
+
+	return this;
+}
+util.inherits(velbus, events.EventEmitter);
+
+velbus.prototype.process_data = function() {
+	while( this.n_unprocessed_data > 6 ) {
+		this.process_message();
+	}
+}
+
+velbus.prototype.process_message = function() {
+	if( this.unprocessed_data[0] != 0x0f ) { this.resync("STX"); return; }
+	if( (this.unprocessed_data[1] & 0xfc) != 0xf8 ) { this.resync("STX1"); return; }
+	var priority = this.unprocessed_data[1] & 0x03;
+	var address = this.unprocessed_data[2];
+	var rtr = (this.unprocessed_data[3] & 0x40) >> 6;
+	var length = this.unprocessed_data[3] & 0x0f;
+	if( this.n_unprocessed_data < 6+length ) { return; }
+	var data = new Buffer(length);
+	this.unprocessed_data.copy(data, 0, 4, 4+length);
+	var checksum = this.unprocessed_data[4+length];
+	if( this.unprocessed_data[4+length+1] != 0x04 ) { this.resync("ETX"); return; }
+
+	var sum = 0;
+	for( var i = 0; i < 4+length; i++ ) {
+		sum += this.unprocessed_data[i];
+	}
+	sum = (-sum)&0xff;
+	if( checksum != sum ) { this.resync("Checksum calc("+sum+")!=data("+checksum+")"); }
+
+	this.unprocessed_data.copy( this.unprocessed_data, 0, 6+length );
+	this.n_unprocessed_data -= 6+length;
+
+	this.emit('message', "Rx data: Prio=" + priority +
+			" Addr=0x" + address +
+			" RTR=" + rtr +
+			" data[" + length + "]=" + hexdump(data)
+			);
+}
+
+velbus.prototype.resync = function(where) {
+	this.emit('resync', where);
+	this.unprocessed_data.copy(this.unprocessed_data, 0, 1); this.n_unprocessed_data--;
+	while( this.n_unprocessed_data > 0 && this.unprocessed_data[0] != 0x0f ) {
+		this.unprocessed_data.copy(this.unprocessed_data, 0, 1); this.n_unprocessed_data--;
+	}
+}
+
+exports.ctor = function(host, port) {
+	return new velbus(host, port);
+}
+
+function hexdump(data) {
+	var h = '';
+	for( var i = 0; i < data.length; i++ ) {
+		h += data[i].toString(16) + ' ';
+	}
+	return h;
+}
--- www/statepush/velbusweb.js
+++ www/statepush/velbusweb.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 var util = require('util');
-var net = require('net');
 var config = require('./config');
 
 util.log("velbusweb starting");
@@ -11,75 +10,20 @@ process.on('SIGINT', function () {
 	process.exit(0);
 });
 
-var bus = net.connect(config.velbusd.port, config.velbusd.host, function() {
-	util.log("velbus connection open");
-});
-
-bus.on('error', function (e) {
+var velbus = require('./velbus').ctor(config.velbusd.host, config.velbusd.port);
+velbus.on('error', function(e) {
 	util.log("velbus connection error: " + e.syscall + " -> " + e.code);
 });
-
-bus.on('close', function() {
-	util.log("velbus connection closed");
-	setTimeout(function(){
-			bus.connect(config.velbusd.port, config.velbusd.host);
-		}, config.velbusd.reconnect_after_ms);
+velbus.on('connect', function() {
+	util.log('velbus connection opened');
+});
+velbus.on('close', function() {
+	util.log('velbus connection closed');
 });
 
-var unprocessed_data = new Buffer(4096);
-var n_unprocessed_data = 0;
-bus.on('data', function(data) {
-	data.copy(unprocessed_data, n_unprocessed_data);
-	n_unprocessed_data += data.length;
-	process_data();
+velbus.on('resync', function(why) {
+	util.log('velbus connection: lost sync: ' + why + ', resyncing');
 });
-
-function process_data() {
-	while( n_unprocessed_data > 6 ) {
-		process_message();
-	}
-}
-function resync(where) {
-	util.log("Lost sync (" + where + "), resyncing");
-	unprocessed_data.copy(unprocessed_data, 0, 1); n_unprocessed_data--;
-	while( n_unprocessed_data > 0 && unprocessed_data[0] != 0x0f ) {
-		unprocessed_data.copy(unprocessed_data, 0, 1); n_unprocessed_data--;
-	}
-}
-function process_message() {
-	if( unprocessed_data[0] != 0x0f ) { resync("STX " + unprocessed_data[0]); return; }
-	if( (unprocessed_data[1] & 0xfc) != 0xf8 ) { resync("STX1 " + unprocessed_data[1]); return; }
-	var priority = unprocessed_data[1] & 0x03;
-	var address = unprocessed_data[2];
-	var rtr = (unprocessed_data[3] & 0x40) >> 6;
-	var length = unprocessed_data[3] & 0x0f;
-	if( n_unprocessed_data < 6+length ) { return; }
-	var data = new Buffer(length);
-	unprocessed_data.copy(data, 0, 4, 4+length);
-	var checksum = unprocessed_data[4+length];
-	if( unprocessed_data[4+length+1] != 0x04 ) { resync("ETX"); return; }
-
-	var sum = 0;
-	for( var i = 0; i < 4+length; i++ ) {
-		sum += unprocessed_data[i];
-	}
-	sum = (-sum)&0xff;
-	if( checksum != sum ) { resync("Checksum calc("+sum+")!=data("+checksum+")"); }
-
-	unprocessed_data.copy( unprocessed_data, 0, 6+length );
-	n_unprocessed_data -= 6+length;
-
-	util.log("Rx data: Prio=" + priority +
-			" Addr=0x" + address +
-			" RTR=" + rtr +
-			" data[" + length + "]=" + hexdump(data)
-			);
-}
-
-function hexdump(data) {
-	var h = '';
-	for( var i = 0; i < data.length; i++ ) {
-		h += data[i].toString(16) + ' ';
-	}
-	return h;
-}
+velbus.on('message', function(data) {
+	util.log('velbus connection: message: ' + data);
+});
