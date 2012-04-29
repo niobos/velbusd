0caa886f70abf6cbf8bf5ab3c8b48d603995c15f
Niobos <niobos@dest-unreach.be>
2012-04-29 10:29:52 +0200

    Moved control routes into their own file

---
+++ www/statepush/controls/blind.js
@@ -0,0 +1,82 @@
+
+exports.add_routes = function(webapp, velbus, vbm, config) {
+
+webapp.post(/\/control\/blind\/([0-9a-fA-F]{2}).([1-4])\/([a-zA-Z ]*)$/, function(req, res, next) {
+	var id = parseInt( req.params[0], 16 );
+	var blind = parseInt( req.params[1] );
+	var field = req.params[2];
+
+	var blindbit = String.fromCharCode( 3 << (blind-1)*2 );
+
+	// We expect a single value in the body
+	var value = Object.keys( req.body )
+	if( value.length != 1 ) {
+		res.send("Expecting single value in POST body", 400);
+		return;
+	}
+	value = value[0];
+
+	switch( field ) {
+	case "status":
+		var command;
+		switch( value ) {
+		case "up":
+			command = "\x05" + blindbit + "\0\0\0"; // Use dip switch settings
+			break;
+
+		case "down":
+			command = "\x06" + blindbit + "\0\0\0";
+			break;
+
+		case "stop":
+			command = "\x04" + blindbit;
+			break;
+
+		default:
+			res.send("Unknown status", 400);
+			return;
+		}
+		velbus.send_message(0, id, 0, command);
+		break;
+
+	default:
+		res.send("Not implemented", 501);
+		return;
+	}
+
+	// And fall through to the GET response
+	req.method = "GET";
+	next();
+});
+
+webapp.get(/\/control\/blind\/([0-9a-fA-F]{2}).([1-4])(?:\/([a-zA-Z ]*))?$/, function(req, res, next) {
+	var id = parseInt( req.params[0], 16 );
+	var blind = parseInt( req.params[1] );
+	var field = req.params[2];
+
+	// Set up listener for the answer
+	var timeout;
+	var send_answer = function(msg) {
+		clearTimeout(timeout);
+		if( field != undefined && field != '' ) {
+			if( msg.hasOwnProperty(field) ) {
+				res.send( msg[field].toString(), {'Content-Type': 'text/plain'} );
+			} else {
+				res.send("Unknown property", 404);
+			}
+		} else {
+			res.send(msg);
+		}
+	};
+	vbm.once('blind status ' + id + '.' + blind, send_answer);
+	timeout = setTimeout(function() {
+			vbm.removeListener('blind status ' + id + '.' + blind, send_answer);
+			res.send("Timeout", 500);
+		}, config.webapp.timeout);
+
+	// Now send the request
+	var blindbit = String.fromCharCode( 3 << (blind-1)*2 );
+	velbus.send_message(3, id, 0, "\xfa" + blindbit );
+});
+
+}
---
+++ www/statepush/controls/relay.js
@@ -0,0 +1,78 @@
+
+exports.add_routes = function(webapp, velbus, vbm, config) {
+
+webapp.post(/\/control\/relay\/([0-9a-fA-F]{2}).([1-4])\/([a-zA-Z ]*)$/, function(req, res, next) {
+	var id = parseInt( req.params[0], 16 );
+	var relay = parseInt( req.params[1] );
+	var field = req.params[2];
+
+	var relaybit = String.fromCharCode( 1 << (relay-1) );
+
+	// We expect a single value in the body
+	var value = Object.keys( req.body )
+	if( value.length != 1 ) {
+		res.send("Expecting single value in POST body", 400);
+		return;
+	}
+	value = value[0];
+
+	switch( field ) {
+	case "status":
+		var command;
+		switch( value ) {
+		case "on":
+			command = "\x02" + relaybit;
+			break;
+
+		case "off":
+			command = "\x01" + relaybit;
+			break;
+
+		default:
+			res.send("Unknown status", 400);
+			return;
+		}
+		velbus.send_message(0, id, 0, command);
+		break;
+
+	default:
+		res.send("Not implemented", 501);
+		return;
+	}
+
+	// And fall through to the GET response
+	req.method = "GET";
+	next();
+});
+
+webapp.get(/\/control\/relay\/([0-9a-fA-F]{2}).([1-4])(?:\/([a-zA-Z ]*))?$/, function(req, res, next) {
+	var id = parseInt( req.params[0], 16 );
+	var relay = parseInt( req.params[1] );
+	var field = req.params[2];
+
+	// Set up listener for the answer
+	var timeout;
+	var send_answer = function(msg) {
+		clearTimeout(timeout);
+		if( field != undefined && field != '' ) {
+			if( msg.hasOwnProperty(field) ) {
+				res.send( msg[field].toString(), {'Content-Type': 'text/plain'} );
+			} else {
+				res.send("Unknown property", 404);
+			}
+		} else {
+			res.send(msg);
+		}
+	};
+	vbm.once('relay status ' + id + '.' + relay, send_answer);
+	timeout = setTimeout(function() {
+			vbm.removeListener('relay status ' + id + '.' + relay, send_answer);
+			res.send("Timeout", 500);
+		}, config.webapp.timeout);
+
+	// Now send the request
+	var relaybit = String.fromCharCode( 1 << (relay-1) );
+	velbus.send_message(3, id, 0, "\xfa" + relaybit );
+});
+
+}
