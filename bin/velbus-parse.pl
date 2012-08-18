5b6f5def5f1d73a0b100ddc9bb64ed3d88e5f010
Niobos <niobos@dest-unreach.be>
2012-08-18 14:40:53 +0200

    Added BlindStatus to perl

--- bin/velbus-parse.pl
+++ bin/velbus-parse.pl
@@ -3,23 +3,30 @@
 use strict;
 use warnings;
 use Data::ParseBinary;
+use Data::Dumper;
 
 
 sub binary ($) {
 	return join '', unpack "B8", pack "C", $_[0];
 }
 
+sub enum {
+	my ($key, %hash) = @_;
+	return $hash{$key} if defined $hash{$key};
+	return sprintf "Unknown[0x%02x]", $key;
+}
+
 my @parser;
 
-push @parser, sub { my ($prio, $addr, $rtr, @data) = @_;
-	# Module Type Request
+push @parser, sub { # Module Type Request {{{
+	my ($prio, $addr, $rtr, $data, @data) = @_;
 	return undef unless $rtr  == 1;
 	return undef unless @data == 0;
 	return sprintf("ModuleTypeRequest to 0x%02x", $addr);
-};
+}; # }}}
 
-push @parser, sub { my ($prio, $addr, $rtr, @data) = @_;
-	# Push Button Status
+push @parser, sub { # Push Button Status {{{
+	my ($prio, $addr, $rtr, $data, @data) = @_;
 	return undef unless $rtr  == 0;
 	return undef unless @data == 4;
 	return undef unless $data[0] == 0x00;
@@ -31,7 +38,49 @@ push @parser, sub { my ($prio, $addr, $rtr, @data) = @_;
 	return sprintf("PushButtonStatus from 0x%02x: on=%s long=%s off=%s",
 			$addr, binary($just_pressed), binary($just_released),
 			binary($long_pressed));
-};
+}; # }}}
+
+push @parser, sub { # Push Button Status {{{
+	my ($prio, $addr, $rtr, $data, @data) = @_;
+	return undef unless $rtr  == 0;
+	return undef unless @data == 4;
+	return undef unless $data[0] == 0x00;
+
+	my $just_pressed = $data[1];
+	my $just_released = $data[2];
+	my $long_pressed = $data[3];
+
+	return sprintf("PushButtonStatus from 0x%02x: on=%s long=%s off=%s",
+			$addr, binary($just_pressed), binary($just_released),
+			binary($long_pressed));
+}; # }}}
+
+push @parser, sub { # Blind Status {{{
+	my ($prio, $addr, $rtr, $data, @data) = @_;
+	return undef unless $rtr  == 0;
+	return undef unless @data == 8;
+	return undef unless $data[0] == 0xec;
+
+	my $channel = enum(  $data[1], 0x03=> "1", 0x0c => "2" );
+	my $timeout = enum(  $data[2], 0=>"15s", 1=>"30s", 2=>"1m", 3=>"2m");
+	my $status1 = enum(  $data[3]&0x03,        0=>"off", 1=>"up", 2=>"down");
+	my $status2 = enum( ($data[3]&0x0c) >> 2,  0=>"off", 1=>"up", 2=>"down");
+	my $led = enum( $data[4],
+				0 => "off",
+				0x80 => "down on", 0x40 => "down slow blink",
+				0x20 => "down fast blink", 0x10 => "down very fast blink",
+				0x08 => "up on", 0x04 => "up slow blink",
+				0x02 => "up fast blink", 0x01 => "up very fast blink"
+			);
+	my $timer = ($data[5] << 16) + ($data[6] << 8) + $data[7];
+
+	return sprintf("BlindStatus from 0x%02x: Blind=%s Timeout=%s " .
+			"Status1=%s Status2=%s Led=%s Timer=%d",
+			$addr, $channel, $timeout, $status1, $status2, $led, $timer);
+}; # }}}
+
+
+
 
 
 $| = 1; # disable buffering
@@ -46,9 +95,10 @@ while(<>) {
 	$addr = hex $addr;
 	$rtr = ( $rtr eq "1" ? 1 : 0 );
 	my @data = map { hex $_ } split / /, $data;
+	$data = join '', map { chr($_); } @data;
 
 	for my $p (@parser) {
-		my $temp = &{$p}($prio, $addr, $rtr, @data);
+		my $temp = &{$p}($prio, $addr, $rtr, $data, @data);
 		if( defined $temp ) {
 			$_ = "$timestamp $source : $temp\n";
 			last; # `for` iteration
@@ -58,3 +108,5 @@ while(<>) {
 } continue {
 	print $_;
 }
+
+# vim: set foldmethod=marker:
