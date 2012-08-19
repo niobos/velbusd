22c74f6e6e43bb29e4362f4e065be8df5eb48c09
Niobos <niobos@dest-unreach.be>
2012-08-19 15:32:30 +0200

    Use extra temp info

--- www/public/js/controls/temp.js
+++ www/public/js/controls/temp.js
@@ -21,15 +21,19 @@ Temp.prototype.show = function() {
 
 	p.append('<div class="temp">' +
 			'<div style="padding-bottom: 0.2em; white-space: nowrap;">' +
-				'Current state: <span class="state">' +
+				'Current temperature: <span class="temp">' +
 					'<img src="images/loading.gif"/></span>' +
 				' (<a href="graph/temp.html?id=' + this.addr + '">' +
 					'history</a>)</div>' +
+			'<div style="padding-bottom: 0.2em; white-space: nowrap;">' +
+				'Current state: <span class="state">' +
+					'<img src="images/loading.gif"/></span></div>' +
 		'</div>');
 };
 
 Temp.prototype.update = function(attr) {
 	var d = Unknown.prototype.update.call(this, attr);
+	var now = +new Date();
 
 	switch( this.state.output.valve ) {
 	case "on":	this.div.addClass('heating'); break;
@@ -38,7 +42,32 @@ Temp.prototype.update = function(attr) {
 
 	if( d == "not displayed" ) return d;
 
-	this.div.find('div.temp span.state').text( this.state.temperature + 'ºC' );
+	this.div.find('div.temp span.temp').text( this.state.temperature + 'ºC' );
+
+	var that = this;
+	var update_state = function() {
+		var reason = '?';
+		if( that.state['schedule_mode'] == "run" ) {
+			if( that.state['prog_temp_mode'] == that.state['temp_mode'] ) {
+				reason = "schedule";
+			} else {
+				reason = "override"; // Until next program step
+			}
+		} else if( that.state['schedule_mode'] == "sleep timer" ) {
+			var t = that.state['sleep_timer'].value * 60;
+			t -= ( now-my_server_time_delta - that.state['sleep_timer'].ref ) / 1000;
+			reason = "override for " + sec_to_pretty(t);
+		} else if( that.state['schedule_mode'] == "manual" ) {
+			reason = "manual";
+		}
+		that.div.find('div.temp span.state').text( that.state['temp_mode'] + ', ' +
+				reason);
+	};
+	clearInterval( this.interval.state ); this.interval.state = undefined;
+	if( this.state['schedule_mode'] == "sleep timer" ) {
+		this.interval.state = setInterval(update_state, 1000);
+	}
+	update_state();
 }
 
 constructors["temp"] = Temp;
