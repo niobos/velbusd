c8224a0e1d50b259faa2ce77bef853c875bd1534
Niobos <niobos@dest-unreach.be>
2012-08-29 14:57:45 +0200

    Bug: negative temperatures are not displayed correctly

--- test/Parser-SensorTemp.sh
+++ test/Parser-SensorTemp.sh
@@ -7,3 +7,11 @@ if [ $? -ne 0 ]; then
 	echo "Wrong output: $OUTPUT"
 	exit 1
 fi
+
+INPUT="2012-01-01T00:00:00+0000 SERIAL : $( echo -ne "\x0f\xfb\x85\x07\xe6\xae\xe0\x2b\x80\x2f\x20\xfc\x04" | ./Parser )"
+OUTPUT="$( echo "$INPUT" | ../bin/velbus-parse.pl )"
+test "$OUTPUT" == "2012-01-01T00:00:00+0000 SERIAL : SensorTemp from 0x85: Current=-40.56C Min=21.75C Max=23.56C"
+if [ $? -ne 0 ]; then
+	echo "Wrong output: $OUTPUT"
+	exit 1
+fi
--- test/Parser-TempSensorStatus.sh
+++ test/Parser-TempSensorStatus.sh
@@ -7,3 +7,11 @@ if [ $? -ne 0 ]; then
 	echo "Wrong output: $OUTPUT"
 	exit 1
 fi
+
+INPUT="2012-01-01T00:00:00+0000 SERIAL : $( echo -ne "\x0f\xfb\x08\x08\xea\x01\x00\x40\xc4\x0a\x00\x00\xed\x04" | ./Parser )"
+OUTPUT="$( echo "$INPUT" | ../bin/velbus-parse.pl )"
+test "$OUTPUT" == "2012-01-01T00:00:00+0000 SERIAL : TempSensorStatus from 0x08: PB=locked Mode=run AutoSend=off Mode=safe Heater Unjam={no_pump no_valve} Program={no_all no_zone no_sensor} ProgramStep=safe Output={no_valve no_boost no_day no_cooler no_pump no_low_alarm high_alarm} Temp=-30.0C Target=5.0C Timer=0"
+if [ $? -ne 0 ]; then
+	echo "Wrong output: $OUTPUT"
+	exit 1
+fi
